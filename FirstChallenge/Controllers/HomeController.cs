c6cbc814230d58b92a104aadaf3f31514a380274
User <User@DESKTOP-GBNQMOH>
2017-09-12 10:48:12 -0700

    Add project files.

---
+++ FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using FirstChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+			List<ComicBook> comics = ComicBookManager.GetComicBooks();
+            return View(comics);
+        }
+
+		public ActionResult Detail(int id)
+		{
+			List<ComicBook> comics = ComicBookManager.GetComicBooks();
+			ComicBook comic = comics.FirstOrDefault(p => p.ComicBookId == id);
+			return View(comic);
+		}
+    }
+}
